c1cfaa2b5cfc0de5d15aca929676042f18c85073
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-10 23:11:28 +0700

    [GREEN] add implementation for Catalog UI

---
+++ __mocks__/api/catalogMocks.ts
@@ -0,0 +1,47 @@
+import { AxiosResponse } from 'axios';
+
+export const catalogPaginationSuccessResponse = {
+  status: 200,
+  message: 'success',
+  data: {
+    pagination: {
+      limit: 5,
+      page: 1,
+      first_url: 'string',
+      last_url: 'string',
+      next_url: 'string',
+      previous_url: 'string',
+      total_page: 5,
+    },
+    items: [
+      {
+        id: 0,
+        name: 'mock_item_name_0',
+        image: '/images/tenda.jpg',
+        price: 10000,
+        description: 'mock_item_descriptions_0',
+      },
+      {
+        id: 1,
+        name: 'mock_item_name_1',
+        image: '/images/tenda.jpg',
+        price: 10000,
+        description: 'mock_item_descriptions_1',
+      },
+      {
+        id: 2,
+        name: 'mock_item_name_2',
+        image: '/images/tenda.jpg',
+        price: 10000,
+        description: 'mock_item_descriptions_2',
+      },
+      {
+        id: 3,
+        name: 'mock_item_name_3',
+        image: '/images/tenda.jpg',
+        price: 10000,
+        description: 'mock_item_descriptions_3',
+      },
+    ],
+  },
+};
---
+++ components/Catalog/SearchBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'twin.macro';
+
+const SearchBar: React.FC = () => {
+  return (
+    <>
+      <div tw="flex items-center justify-center">
+        <div tw="mx-4 flex border-2 rounded w-full">
+          <input type="text" tw="w-10/12 px-4 py-2" placeholder="Search..." />
+          <button tw="w-2/12 flex items-center justify-center border-l">
+            <svg
+              tw="w-6 h-6 text-gray-600"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
